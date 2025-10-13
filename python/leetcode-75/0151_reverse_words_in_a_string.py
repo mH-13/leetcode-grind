424290fc38296dd1dd297c05c2d63fcbc2da8c58
mH-13 <mh13.mehedi.hasan@gmail.com>
2025-10-13 16:46:19 +0600

    0151

--- python/leetcode-75/0151_reverse_words_in_a_string.py
+++ python/leetcode-75/0151_reverse_words_in_a_string.py
@@ -7,4 +7,28 @@ Link: https://leetcode.com/problems/reverse-words-in-a-string/
 """
 
 class Solution:
-    pass
+    def reverseWords(self, s: str) -> str:
+        return " ".join(reversed(s.split())) 
+    # In short, split the string by whitespace, reverse the list of words, and join them with a single space.
+
+        """# Alternative approach using two pointers
+        n = len(s)
+        left, right = 0, n - 1
+        # Trim leading spaces
+        while left <= right and s[left] == ' ':
+            left += 1
+        # Trim trailing spaces
+        while left <= right and s[right] == ' ':
+            right -= 1  
+        # Reverse the entire string
+        s = list(s[left:right + 1])
+        reverse(s, 0, len(s) - 1)
+        # Reverse each word in the reversed string
+        start = 0
+        for i in range(len(s)):
+            if s[i] == ' ':
+                reverse(s, start, i - 1)
+                start = i + 1
+        reverse(s, start, len(s) - 1)
+        return ''.join(s)   """
+    

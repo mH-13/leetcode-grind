446cc06473ce62097af442418e60b25bb7231691
mH-13 <mh13.mehedi.hasan@gmail.com>
2025-10-21 10:59:21 +0600

    1431

--- python/leetcode-75/1431_kids_with_the_greatest_number_of_candies.py
+++ python/leetcode-75/1431_kids_with_the_greatest_number_of_candies.py
@@ -5,6 +5,31 @@ Time: O() | Space: O()
 Tags: Array
 Link: https://leetcode.com/problems/kids-with-the-greatest-number-of-candies/
 """
-
+from typing import List
 class Solution:
-    pass
+    def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
+        # newlist = list(candies)
+        newlist= sorted(candies, reverse = True)
+        # print(candies)
+        # print(newlist[0])
+        mx = newlist[0]
+        n = len(candies)
+        res = [False] * n
+        for i in range(0, n):
+            check = candies[i] + extraCandies
+            # print(candies[i], check)
+            if (check>= mx):
+                res[i] = True
+        return res
+
+        
+
+# from typing import List
+# class Solution:
+#     def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
+#         mx = max(candies)
+#         return [candy + extraCandies >= mx for candy in candies]
+
+if __name__ == "__main__":
+    s = Solution()
+    print(s.kidsWithCandies([2,3,5,1,3], 3))
